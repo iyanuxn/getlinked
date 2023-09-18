f336b09706c54ac87d88367a78bbb51345c1a5e2
Iyanu <iyanuxn@gmail.com>
2023-09-18 18:06:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      {" "}
+      <Navbar />
+      <div>App</div>
+    </>
+  );
+};
+
+export default App;
