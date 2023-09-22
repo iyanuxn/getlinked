e4c52359f2124cbb9619a0d4a043128cd88cc9d5
Iyanu <iyanuxn@gmail.com>
2023-09-22 14:54:48 +0100

    Basic completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,55 @@
+import axios from "axios";
 import IG from "../assets/Ig.svg";
 import X from "../assets/X.svg";
 import FB from "../assets/facebook.svg";
 import LINKEDIN from "../assets/linkedin.svg";
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert("I was clicked");
+
+    const teamName = e.target.elements.teamName.value.trim();
+    const email = e.target.elements.email.value.trim();
+    const message = e.target.elements.message.value.trim();
+
+    if (!teamName || !email || !message) {
+      alert("Please fill in all fields");
+      return false;
+    }
+
+    const formData = {
+      first_name: teamName,
+      email: email,
+      message: message,
+    };
+
+    axios
+      .post(
+        "https://backend.getlinked.ai/hackathon/contact-form",
+        JSON.stringify(formData),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        if (response.status === 201) {
+          console.log("formData:", formData);
+          alert("Message sent successfully");
+        } else {
+          alert("Message not sent");
+        }
+      })
+      .catch((error) => {
+        console.log(error.response);
+        console.log(error.message);
+        console.log(error.request);
+      });
+  };
+
   return (
     <div className="text-white md:pt-60 pt-28 md:pb-32 md:px-20 px-8 py-10 regBg min-h-screen font-regular">
       <div className="flex justify-between md:px-12">
@@ -31,7 +77,10 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <form className="md:w-1/2 w-full flex flex-col justify-center items-center md:shadow-2xl md:border mt-6 md:mt-0 border-white border-opacity-5 rounded-md md:py-16 px-5 md:px-20">
+        <form
+          className="md:w-1/2 w-full flex flex-col justify-center items-center md:shadow-2xl md:border mt-6 md:mt-0 border-white border-opacity-5 rounded-md md:py-16 px-5 md:px-20"
+          onSubmit={handleSubmit}
+        >
           <div className="flex flex-col items-start w-full">
             {" "}
             <span className="font-head text-primary font-semibold text-xl text-left">
@@ -44,26 +93,29 @@ const Contact = () => {
           </div>
           <div className="flex flex-col gap-10 mt-8 w-full">
             <input
+              name="teamName"
               type="text"
+              autoComplete="off"
               placeholder="Team's Name"
               className="w-full bg-transparent border border-white bg-white bg-opacity-5 rounded-md py-2 px-4"
             />{" "}
             <input
-              type="text"
-              placeholder="Topic"
-              className="w-full bg-transparent border border-white bg-white bg-opacity-5 rounded-md py-2 px-4"
-            />
-            <input
+              name="email"
               type="email"
               placeholder="Email"
+              autoComplete="off"
               className="w-full bg-transparent border border-white bg-white bg-opacity-5 rounded-md py-2 px-4"
             />
             <textarea
+              name="message"
               placeholder="Message"
               className="w-full bg-transparent border border-white bg-white bg-opacity-5 rounded-md py-2 px-4"
             />
           </div>
-          <button className="md:w-44 w-28 text-sm md:text-base bg-gradient-to-r from-primary to-secondary text-white py-3 rounded-md md:mt-10 mt-8">
+          <button
+            className="md:w-44 w-28 text-sm md:text-base bg-gradient-to-r from-primary to-secondary text-white py-3 rounded-md md:mt-10 mt-8"
+            type="submit"
+          >
             Submit
           </button>
 
